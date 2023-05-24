ce530082e9c3d1d53aade3c0c76b72552bdc8c33
EduardoBirchal <edubirchal@gmail.com>
2023-05-24 08:34:06 -0300

    Terminei wrap na tela

--- Assets/Scripts/Pais/WrapTela.cs
+++ Assets/Scripts/Pais/WrapTela.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class WrapTela : FuncoesGerais
 {
     public float buffer;
-    float larguraFromOrigin, alturaFromOrigin, left, right;
+    float limBaixo, limCima, limEsquerda, limDireita, profundidadeCamera;
     Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-        left = Camera.main.ScreenToWorldPoint( new Vector3(0.0f, 0.0f, 0 - Camera.main.transform.position.z) ).x - buffer;
-        right = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, 0.0f, 0 - Camera.main.transform.position.z) ).x + buffer;
+        profundidadeCamera = Camera.main.transform.position.z;
+
+        limEsquerda = Camera.main.ScreenToWorldPoint ( new Vector3(0.0f, 0.0f, 0 - profundidadeCamera) ).x - buffer;
+        limDireita = Camera.main.ScreenToWorldPoint ( new Vector3(Screen.width, 0.0f, 0 - profundidadeCamera) ).x + buffer;
+
+        limBaixo = Camera.main.ScreenToWorldPoint ( new Vector3(0.0f, 0.0f, 0 - profundidadeCamera) ).y - buffer;
+        limCima = Camera.main.ScreenToWorldPoint ( new Vector3(0.0f, Screen.height, 0 - profundidadeCamera) ).y + buffer;
     }
 
     // Update is called once per frame
     void Update()
     {
         pos = transform.position;
+        checaHorizontal(pos);
+        checaVertical(pos);
+    }
 
-        if(pos.x < left) { 
-            transform.position = new Vector3 (right, pos.y, pos.z); 
+    void checaHorizontal (Vector3 pos) {
+        if(pos.x < limEsquerda) { 
+            transform.position = new Vector3 (limDireita, pos.y, pos.z); 
         }
+        if(pos.x > limDireita) {
+            transform.position = new Vector3 (limEsquerda, pos.y, pos.z); 
+        }
+    }
 
-
+    void checaVertical (Vector3 pos) {
+        if(pos.y < limBaixo) { 
+            transform.position = new Vector3 (pos.x, limCima, pos.z); 
+        }
+        if(pos.y > limCima) {
+            transform.position = new Vector3 (pos.x, limBaixo, pos.z); 
+        }
     }
 }
