8106a814ca6759da37511e6f5837318e8fc6a3f1
caioplays@outloo.com <caioplays@outlook.com>
2023-06-20 10:30:02 -0300

    relate to last commit

--- Assets/Scripts/Net/NetworkStart.cs
+++ Assets/Scripts/Net/NetworkStart.cs
@@ -12,13 +12,17 @@ public class NetworkStart : MonoBehaviour
     [SerializeField] private GameObject clientBtn;
     [SerializeField] private GameObject startBtn;
     [SerializeField] private GameObject spawner;
+    private NetworkManager netManager;
     public static bool isSingleplayer = false;
     public static bool gameStarted = false;
     public int MaxNumPlayers;
 
     private void Start()
     {
-        MaxNumPlayers = isSingleplayer ? 1 : 4;
+        MaxNumPlayers = isSingleplayer ? 2 : 4;
+        netManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
+
         GameObject.Find("NetworkManager").GetComponent<UnityTransport>().ConnectionData.Address = MenuManager.texto_ip;
         if(isSingleplayer == true)
         {
@@ -60,11 +64,44 @@ public class NetworkStart : MonoBehaviour
 
     }
 
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    {
+        var clientId = request.ClientNetworkId;
+        var connectionData = request.Payload;
+    
+        if(netManager.ConnectedClientsIds.Count < MaxNumPlayers)
+        {
+            response.Approved = true;
+            response.CreatePlayerObject = true;
+        }
+        else
+        {
+            // TODO: Mostrar a tela de "sala cheia" e "incapaz de conectar"
+            response.Reason = "Nao foi";
+            response.Approved = false;
+        }
+    
+        // The prefab hash value of the NetworkPrefab, if null the default NetworkManager player prefab is used
+        response.PlayerPrefabHash = null;
+    
+        // If additional approval steps are needed, set this to true until the additional steps are complete
+        // once it transitions from true to false the connection approval response will be processed.
+        response.Pending = false;
+    }
+
     public static string GetLocalIPv4()
     {
         return Dns.GetHostEntry(Dns.GetHostName())
         .AddressList.First(
         f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
         .ToString();
+    }
+
+    private void OnClientDisconnectCallback(ulong obj)
+    {
+        if (!netManager.IsServer && netManager.DisconnectReason != string.Empty)
+        {
+            Debug.Log($"Approval Declined Reason: {netManager.DisconnectReason}");
+        }
     }
 }
