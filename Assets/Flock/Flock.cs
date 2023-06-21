64d824c9cdbdfc9af6658c7f464740c783f4795b
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-06-21 08:30:44 -0300

    Update implementei na cena de multiplayer

--- Assets/Flock/Flock.cs
+++ Assets/Flock/Flock.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class Flock : MonoBehaviour
+public class Flock : FuncoesGerais
 {
+    public int onda = 0, numDificuldades;
+    public float tempoEspera, dificuldade, dificuldadeTotal, tamanhoMargem, alturaTela, larguraTela;
+    public GameObject[] inimigosDif1, inimigosDif2, inimigosDif3;
+    private Vector2 distanciaMargem;
+    private GameObject[][] listaInimigos;
+    private GameObject texto;
+    private FuncoesTexto funcoesTexto;
+    private NetworkStart NetworkInfo;
+
+
+
+
     public FlockAgent agentPrefab;
     List<FlockAgent> agents = new List<FlockAgent>();
     public FlockBehavior behavior;
 
-    [Range(10, 500)]
-    public int startingCount = 250;
-    const float AgentDensity = 0.08f;
 
     [Range(1f, 100f)]
     public float driveFactor = 10f;
@@ -33,18 +43,19 @@ public class Flock : MonoBehaviour
         squareNeighborRadius = neighborRadius * neighborRadius;
         squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
 
-        for (int i = 0; i < startingCount; i++)
-        {
-            FlockAgent newAgent = Instantiate(
-                agentPrefab,
-                Random.insideUnitCircle * startingCount * AgentDensity,
-                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
-                transform
-                );
-            newAgent.name = "Agent " + i;
-            newAgent.Initialize(this);
-            agents.Add(newAgent);
-        }
+
+        NetworkInfo = GameObject.Find("NetworkManager").GetComponent<NetworkStart>();
+        gameObject.name = "SpawnerInimigo";
+        distanciaMargem = new Vector2(larguraTela - tamanhoMargem, alturaTela - tamanhoMargem);
+        dificuldadeTotal *= dificuldade;
+        listaInimigos = new GameObject[][] {inimigosDif1};
+        
+        texto = GameObject.Find("TextoGrandeMapa");
+        funcoesTexto = texto.GetComponent<FuncoesTexto>();
+        if(IsHost)
+            StartCoroutine(ChecaInimigos());
+
+        
     }
 
     // Update is called once per frame
@@ -80,5 +91,91 @@ public class Flock : MonoBehaviour
         }
         return context;
     }
+
+
+
+
+
+
+
+    IEnumerator ChecaInimigos() {
+        // Procura todos os objetos com a tag "Inimigo". Se não tiver inimigos, cria uma nova onda
+        GameObject[] inimigos = GameObject.FindGameObjectsWithTag("Inimigo");
+
+        if(inimigos.Length == 0 && NetworkStart.gameStarted == true) {
+            StartCoroutine(CriaOnda());
+        }
+        else {
+            // Espera 1 segundo e executa a função de novo. Não está usando Update porque só precisa rodar 1 vez por segundo em vez de 1 vez por frame.
+            // Não reinicia toda vez porque o CriaOnda() espera um tempo antes de spawnar os inimigos. Se fizesse a checagem durante esse tempo, ia 
+            // criar várias ondas antes da primeira spawnar.
+            yield return new WaitForSeconds(tempoEspera);
+            StartCoroutine(ChecaInimigos());
+        }
+    }
+
+    void SpawnaInimigo(int i) {
+        float posX, posY;
+
+        if(Random.Range(0, 2) < 1) { // 50% de chance de X ser amplo e Y ser restrito
+            posX = SinalAleatorio() * Random.Range(0, larguraTela);
+            posY = SinalAleatorio() * Random.Range(alturaTela - tamanhoMargem, alturaTela);
+        }
+        else {                       // E 50% de chance do contrário
+            posX = SinalAleatorio() * Random.Range(larguraTela - tamanhoMargem, larguraTela);
+            posY = SinalAleatorio() * Random.Range(0, alturaTela);
+        }
+
+        Vector3 posicaoSpawn = new Vector3 (posX, posY, 0);
+        FlockAgent novoInimigo = Instantiate(agentPrefab, posicaoSpawn, Quaternion.identity);
+        novoInimigo.GetComponent<NetworkObject>().Spawn();
+
+        novoInimigo.name = "Agent " + i;
+        novoInimigo.Initialize(this);
+        agents.Add(novoInimigo);
+
+    }
+
+    
+
+    [ServerRpc]
+    void IniciarChecaInimigosServerRpc()
+    {
+        Debug.LogWarning(IsServer);
+        StartCoroutine(ChecaInimigos());
+    }                                                    
+    
+    [ClientRpc]
+    void MostrarOndaClientRpc(int onda)
+    {
+        funcoesTexto.MostraFade(1.5f, 1.5f, "Onda " + onda);
+    }
+    IEnumerator CriaOnda() {
+        yield return new WaitForSeconds(2);
+        int dificuldadeDisponivel = (int) dificuldadeTotal;
+        onda++;
+
+        Debug.LogWarning(IsServer);
+        MostrarOndaClientRpc(onda);
+
+        int i = 0;
+        while (dificuldadeDisponivel > 0) {
+            // Escolhe o menor número entre numDificuldades e dificuldadeDisponivel
+            int dificuldadeAtual = Random.Range(1, dificuldadeDisponivel > numDificuldades ? numDificuldades : dificuldadeDisponivel); 
+
+            // Escolhe um inimigo aleatório na dificuldade escolhida
+            int indexInimigo = Random.Range(0, listaInimigos[dificuldadeAtual-1].Length - 1);
+
+            SpawnaInimigo(i);
+            dificuldadeDisponivel -= dificuldadeAtual;
+
+            i++;
+
+            yield return new WaitForSeconds(0.5f);
+        }   
+
+        dificuldadeTotal++;
+        StartCoroutine(ChecaInimigos());                                                                         
+    } 
 
 }
